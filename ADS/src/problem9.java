bcbe15646de8ac12e43e71d0c0f4b0fa534bd24e
diiwkk <129244932+diiwkk@users.noreply.github.com>
2023-04-09 15:31:53 +0600

    9

---
+++ ADS/src/problem9.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class problem9 {
+    public static int binomialCoefficient(int n, int k) {
+        if (k == 0 || k == n) {
+            return 1;
+        }
+        return binomialCoefficient(n - 1, k - 1) + binomialCoefficient(n - 1, k);
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int k = input.nextInt();
+        input.close();
+        int result = binomialCoefficient(n, k);
+        System.out.println(result);
+    }
+}
